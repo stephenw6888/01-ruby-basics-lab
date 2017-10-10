05b6273f7e82af3f21bbe01819270b0351b44c38
stephenw6888 <stephenw6888@hstat.org>
2017-10-10 23:10:18 +0000

    Finish Lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,24 @@
-def division
+def division(ori_val,tot_val)
+    ori_val/tot_val
+end
+
+def argue(phrase)
+   "#{phrase}"
+end
+
+def greeting(greet, name)
+    "#{greet} #{name}"
     
-end
-
-def argue
-  
-end
-
-def greeting
-
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(topping="cheese")                    
+    topping
 end
-
-def whisper
-    
+                    
+def whisper(word)
+    "#{word.downcase}"
 end
